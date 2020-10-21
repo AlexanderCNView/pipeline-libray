33d8ff5a13f9c4352cf597c79033233dbbf3c6b6
Rabbit <50302410@qq.com>
2020-10-21 15:44:25 +0800

    no message

--- vars/pipeline.groovy
+++ vars/pipeline.groovy
@@ -3,6 +3,12 @@ def call(params) {
     // 让我们来执行个echo命令
     pipeline {
         agent any
+        parameters {
+            //git代码路径【参数值对外隐藏】
+            string(name:'repoUrl', defaultValue: 'git@git.*****.com:*****/*****.git', description: 'git代码路径')
+            //repoBranch参数后续替换成git parameter不再依赖手工输入,JENKINS-46451【git parameters目前还不支持pipeline】
+            string(name:'repoBranch', defaultValue: 'master', description: 'git分支名称')
+        }
         stages {
             stage("第一步") {
                 steps {
